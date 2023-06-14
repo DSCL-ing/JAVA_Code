454c451d7a9184f175aec4f1a618c18c84ac597c
DSCL-ing <2639075023@qq.com>
2023-06-14 10:55:29 +0800

    upate

--- TextDemo/src/异常/Test.java
+++ TextDemo/src/异常/Test.java
@@ -1,5 +1,8 @@
 package 异常;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Test {
 
     /** note
@@ -71,44 +74,45 @@ public class Test {
     }
 
     //java.lang.ArrayIndexOutOfBoundsException :数组越界异常
-    public static void main(String[] args) {//3
+    public static void main3(String[] args) {//3
         //IndexOutOfBoundsException
         int[] array = {1, 2, 3};
         System.out.println(array[10]);
     }
 
     //异常的处理
+
     /**
      * 1.防御式编程 LBYL:Look Before You Leap
      * 每一步都做检查.做下一步前必须保证每一步都是成功的.
      * 缺陷:正常流程和错误处理流程代码混在一起,代码整体显得比较混乱
-     *
+     * <p>
      * 2.事后认错型 EAFP:It's Easier to Ask Forgivenss than permission
      * 先操作,遇到问题再集中处理
      * 优势:正常流程和错误流程是分离开的,代码更清晰,更容易理解
-     *
+     * <p>
      * 异常处理的核心思想就是EAFP
-     *
+     * <p>
      * java中,异常处理主要的5个关键字:throw,try,catch,final,throws
-     *
+     * <p>
      * 异常处理的前提:触发/抛出异常
      * 1.代码自己执行过程中触发异常
      * 2.自己手动抛出异常:throw:扔
-     *
+     * <p>
      * (抛出异常的信息:最上面/第一条是直接抛出异常的位置,其他地方是间接位置)
      * 抛出的异常是一个类
      * 抛出异常后,如果不经过任何处理,则遇到任何异常都会交给JVM处理:终止程序
-     *
+     * 不会同时抛出多个异常
+     * <p>
      * throw:
      * 1.一般都是用在自己自定义的异常上
      * 2.throw必须写在方法体内部
      * 3.抛出的对象必须是Exception或者Excepion的子类
      * 4.如果抛出的是RunTimeException或者RunTimeException的子类,则可以不用处理,直接交给JVM来处理
      * 5.如果抛出的是编译时异常,用户必须处理,否则无法通过编译
-     * 6.异常一旦抛出,其后的代码就不再执行
+     * 6.异常一旦抛出,其后的代码就不再执行(捕获到了就直接跳到异常处理)
      *
      * 异常的捕获:主要有两种:异常声明throws和try-catch捕获处理
-     *
      *
      * throws:声明抛出的异常(解决受查异常)
      * 说明:处在方法声明的参数列表之后,当方法中抛出编译时异常,用户不想处理改异常,此时就可以借助throws将异常
@@ -117,8 +121,35 @@ public class Test {
      * 2.声明的异常必须是Exception或者Exception的子类
      * 3.方法内部如果抛出了多个异常,throws之后必须跟多个异常类型,之间用逗号隔开,如果抛出多个异常类型具有父子关系
      * ,则直接声明父类即可
+     */
+
+    //try和catch和finally
+    /**
+     * 捕获异常与catch的顺序无关,抛出哪个异常就执行哪个catch(catch的书写不影响异常的捕获
      *
      */
+    public static void main(String[] args) {//4
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int[] array = null;
+            System.out.println(array.length);
+            System.out.println("xxxxxxxx");//会被跳过
+            //多个异常用|分隔
+            //缺点:不知道哪个异常出现了问题
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            System.out.println("捕获到了NullPointerException异常");
+        //父类不能在子类之前,否则报错.因为接下来子类异常没有任何意义
+        } catch (Exception e) {
+            System.out.println("捕获所有异常");
+        }finally{//一般用于资源回收
+            scanner.close();
+            System.out.println("finally...");//最终,怎样都会执行,捕获和没有捕获和和没有异常JVM都可以
+            //return 9; //不建议在finally中写return语句,因为finally会被强制执行,可能会破坏原有逻辑
+        }
+        System.out.println("后续流程");
+    }
 
 
 
